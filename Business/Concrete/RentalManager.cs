c1f9e33af499fff4b98bcea390efd88dce40958f
eterzioglu <eterzioglu8@gmail.com>
2021-03-05 22:20:00 +0300

    10.bölüm 4. odev

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,55 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalDal _rentalDal;
+
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+
+        public IResult Add(Rental rental)
+        {
+            if (_rentalDal.Get(r => r.CarId == rental.CarId).ReturnDate == null)
+            {
+                return new ErrorResult("Rental operation failed.");
+            }
+            else
+            {
+                _rentalDal.Add(rental);
+                return new SuccessResult("Rental operation succed.");
+            }
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            _rentalDal.Delete(rental);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll());
+        }
+
+        public IDataResult<Rental> GetById(int rentalId)
+        {
+            return new SuccessDataResult<Rental>(_rentalDal.Get(r=>r.RentalId == rentalId));
+        }
+
+        public IResult Update(Rental rental)
+        {
+            _rentalDal.Update(rental);
+            return new SuccessResult();
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -13,6 +13,37 @@ namespace ConsoleUI
             //CarTest();
             //BrandTest();
             //ColorTest();
+            //CustomerTest();
+            RentalTest();
+            //UserTest();
+        }
+
+        private static void UserTest()
+        {
+            UserManager userManager = new UserManager(new EfUserDal());
+            userManager.Add(new User { UserId = 1, FirstName = "Ebru", LastName = "Terziogu", Email = "eterzioglu8", Password = "12345" });
+        }
+
+        private static void RentalTest()
+        {
+            RentalManager rentalManager = new RentalManager(new EfRentalDal());
+            rentalManager.Add(new Rental { RentalId = 7, CarId = 3, CustomerId = 1});
+
+            //foreach (var rental in rentalManager.GetAll().Data)
+            //{
+             //   Console.WriteLine(rental.RentalId);
+            //}
+        }
+
+        private static void CustomerTest()
+        {
+            CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+            //customerManager.Add(new Customer { CustomerId = 2, UserId = 2, CompanyName = "Allianz" });
+
+            foreach (var customer in customerManager.GetAll().Data)
+            {
+                Console.WriteLine(customer.CompanyName);
+            }
         }
 
         private static void ColorTest()
